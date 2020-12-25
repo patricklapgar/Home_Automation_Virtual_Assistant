d3b59ee0220135f7460d316491d7dc18ba476f76
patricklapgar <patricklapgardeveloper@gmail.com>
2020-12-24 20:49:13 -0600

    Added popup window to display search results to user

--- assistant.py
+++ assistant.py
@@ -1,30 +1,31 @@
 # Patrick Apgar
 
-# import wolframalpha
-
-
-# app_id = '7RQEH5-E64LEQHTY8'  # get your own at https://products.wolframalpha.com/api/
-# client = wolframalpha.Client(app_id)
-
-
-# res = client.query('temperature in Washington, DC on October 3, 2012')
-# print(next(res.results).text)
-
+import wolframalpha
 import PySimpleGUI as sg
 
+app_id = '7RQEH5-E64LEQHTY8'  # get your own at https://products.wolframalpha.com/api/
+client = wolframalpha.Client(app_id)
+
+sg.theme('Topanga')
 # Define the window's contents
-layout = [  [sg.Text("What's your name?")],     # Part 2 - The Layout
+layout = [  [sg.Text("Enter a command")],
             [sg.Input()],
-            [sg.Button('Ok')] ]
+            [sg.Button('Ok'), sg.Button('Cancel')] ]
 
 # Create the window
-window = sg.Window('Window Title', layout)      # Part 3 - Window Defintion
+window = sg.Window('Alexandra', layout)      
 
-# Display and interact with the Window
-event, values = window.read()                   # Part 4 - Event loop or Window.read call
+while True:
+    # Display and interact with the Window
+    event, values = window.read()                  
 
-# Do something with the information gathered
-print('Hello', values[0], "! Thanks for trying PySimpleGUI")
+    if event in (None, 'Cancel'):
+        break
+    # Do something with the information gathered
+    res = client.query(values[0])
+    sg.popup(next(res.results).text) # Create a popup window to display information
+
+    print(next(res.results).text)
 
 # Finish up by removing from the screen
 window.close() 
