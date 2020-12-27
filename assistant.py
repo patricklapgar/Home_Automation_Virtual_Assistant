8c45ef4835fa93698bd10d717e770ce54f6ca902
patricklapgar <patricklapgardeveloper@gmail.com>
2020-12-26 21:50:54 -0600

    Enabled WolframAlpha and Wikipedia functionality. Currently trying to implement speech-to-text functionality as well

--- assistant.py
+++ assistant.py
@@ -3,9 +3,14 @@
 import wolframalpha
 import PySimpleGUI as sg
 import wikipedia
+import pyttsx3
 
-app_id = 'API KEY GOES HERE'  # get your own at https://products.wolframalpha.com/api/
+app_id = '7RQEH5-E64LEQHTY8'  # get your own at https://products.wolframalpha.com/api/
 client = wolframalpha.Client(app_id)
+
+engine = pyttsx3.init()
+engine.say("I will speak this text")
+engine.runAndWait()
 
 sg.theme('Topanga')
 # Define the window's contents
@@ -22,11 +27,14 @@ while True:
 
     if event in (None, 'Cancel'):
         break
+
     # Do something with the information gathered
     res = client.query(values[0])
-    sg.popup(next(res.results).text) # Create a popup window to display information
+    wolfram_response = next(res.results).text
 
-    #print(next(res.results).text)
+    wikipedia_response = wikipedia.summary(values[0], sentences=2)
+    sg.popup("Result from Wolfram Alpha show that " + values[0] + " is " + wolfram_response, "A result from Wikipedia also shows that: " + wikipedia_response) # Create a popup window to display information
 
+    # print(values[0])
 # Finish up by removing from the screen
 window.close() 
