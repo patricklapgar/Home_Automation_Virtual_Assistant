5429488dc80635d7f927a6bf952ea90ac5b6960a
patricklapgar <44283314+patricklapgar@users.noreply.github.com>
2020-12-28 22:10:33 -0600

    Update assistant.py

--- assistant.py
+++ assistant.py
@@ -6,16 +6,8 @@ import wikipedia
 from gtts import gTTS
 from playsound import playsound
 
-app_id = '7RQEH5-E64LEQHTY8'  # get your own at https://products.wolframalpha.com/api/
+app_id = 'YOUR API KEY GOES HERE'  # get your own at https://products.wolframalpha.com/api/
 client = wolframalpha.Client(app_id)
-
-# v = gTTS(text="Hello! Maye I know your name please", lang="en", slow=False)
-# v.save("name.mp3")
-# playsound("name.mp3")
-# name = input("Enter name here: ")
-# s = gTTS(text="hello" + name + " I am Google Text To Speech", lang="en")
-# s.save("welcome.mp3")
-# playsound("welcome.mp3")
 
 sg.theme('Topanga')
 # Define the window's contents
